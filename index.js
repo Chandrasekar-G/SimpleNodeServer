f9dc8629dfd8e6809096e00e9851a49d43f1b2d3
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2018-01-10 23:42:18 +0530

    Send a html file as response

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var http = require("http"),
-    host = "127.0.0.1",
-    port = 8080;
+  fs = require("fs"),
+  host = "127.0.0.1",
+  port = 8080;
 
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.end("Hello World!!!");
-});
+fs.readFile("index.html", function(err, data) {
+  var server = http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
 
-server.listen(port, host, function(){
+  server.listen(port, host, function() {
     console.log("Listening now...");
+  });
 });
